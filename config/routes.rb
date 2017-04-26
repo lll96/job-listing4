0fae48a1c638990167050cdf4326705c9f466503
hy <15871480943@163.com>
2017-04-26 08:34:41 +0800

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   devise_for :users
   namespace :admin do
     resources :jobs do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420203505) do
+ActiveRecord::Schema.define(version: 20170425032927) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170420203505) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+    t.string   "category"
   end
 
   create_table "resumes", force: :cascade do |t|
